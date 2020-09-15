8ce3ecf282778db1e203be0945c596df792beab1
Matthew Viens <matthew.j.viens@gmail.com>
2020-09-15 14:58:14 -0700

    Remove contact from index

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>I'm Andrew, a full-stack developer living in beautiful Philadelphia.</h2>
-      <p>Need a developer? <Link to="/contact">Contact Me</Link>.</p>
     </Layout>
   )
 }
